49b9502d6a5a0a4db77fb51b47d2fe9cf3febcdb
leav <leavthefollower@gmail.com>
2014-11-17 09:36:22 -0800

    smaller loading bar and move "Loading..." to the left

--- javascripts/LoadingBar.js
+++ javascripts/LoadingBar.js
@@ -31,9 +31,9 @@ LoadingBar.prototype.initialize = function(total) {
 	bar.strokeColor = 'rgba(104, 89, 65, 1.0)';
 	bar.fillColor = 'rgba(235, 254, 114, 1.0)';
 	
-	var text = new createjs.Text('Loading', '4em Calibri', bar.strokeColor);
+	var text = new createjs.Text('Loading', '1.5em Calibri', bar.strokeColor);
 	this.addChild(text);
-	text.regX = text.getMeasuredWidth() * 0.8;
+	text.regX = bar.regX;
 	text.regY = -text.getMeasuredHeight() * 0.1;
 	var textBlinkTimeout = 1;
 	var textBlinkCount = 0;
@@ -61,7 +61,7 @@ LoadingBar.prototype.initialize = function(total) {
 			
 		text.x = bar.x = getWindowWidth() / 2;
 		text.y = bar.y = getWindowHeight() / 2;
-		text.scaleX = text.scaleY = bar.scaleX = bar.scaleY = getWindowHeight() / bar.height * 0.10;
+		text.scaleX = text.scaleY = bar.scaleX = bar.scaleY = getWindowHeight() / bar.height * 0.05;
 	}
 	
 	this.doLayout();
